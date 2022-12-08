d84e2f320f94c761af71421a47d8974c951af826
laptinho <camsanbar@gmail.com>
2022-12-08 15:50:06 -0300

    change card width and margin x

--- src/module/renderCard.js
+++ src/module/renderCard.js
@@ -20,7 +20,7 @@ const template = (events, container) => {
 	let html = "";
 	events.forEach(({_id, image, name, description, price}) => {
 		html += `
-      <div class="card col-8 col-sm-5 col-md-4 col-lg-3 col-xl-2 mt-5 bg-dark text-light mx-3 mx-md-5 text-center border border-light px-0">
+      <div class="card col-8 col-sm-5 col-md-4 col-lg-3 col-xl-2 mt-5 bg-dark text-light mx-3 mx-md-4 text-center border border-light px-0">
         <img src="${image}" class="card-img-top w-100" alt="${name}">
         <div class="card-body d-flex flex-column justify-content-between">
           <h5 class="card-title">${name}</h5>
