723d2cc900c1fe1530a08d78c015227f0601d58a
laptinho <camsanbar@gmail.com>
2022-12-15 11:14:07 -0300

    Delete searchFilter condition

--- src/module/searchFilter.js
+++ src/module/searchFilter.js
@@ -1,10 +1,5 @@
-const searchFilter = (
-	value,
-	data
-) => 
-	value.length
-		? data.filter((event) =>
-				event.name.toLowerCase().includes(value.toLowerCase())
-		  )
-		: data
+const searchFilter = (value, data) =>
+	data.filter((event) =>
+		event.name.toLowerCase().includes(value.toLowerCase())
+	);
 export default searchFilter;
