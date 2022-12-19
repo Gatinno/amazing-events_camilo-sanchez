b6b4555e4fc77ff455c03e97195bbad20adf787d
laptinho <camsanbar@gmail.com>
2022-12-19 15:39:00 -0300

    create renderStatics and change stats controler

--- src/controlers/stats.js
+++ src/controlers/stats.js
@@ -1,37 +1,18 @@
+import DataModule from '../module/dataModule.js'
 import getData from '../module/getData.js'
+import { template, renderTableUpcomingPast } from '../module/renderStatics.js'
 document.addEventListener('DOMContentLoaded', async () => {
   const firstTable = document.getElementsByClassName('first-table')[0]
   const upcomingTable = document.getElementById('upcoming-table')
   const pastTable = document.getElementById('past-table')
   const data = await getData("https://amazing-events.onrender.com/api/events")
-  const eventWithHighestCapacity = getEventsWithCapacity(data.events).sort((a, b) => b.capacity - a.capacity)[0]
-  const highestAndLowestEvents = getHighestAndLowestEvents( getEventsWithPercent(data.events).sort((a, b) => b.percentAssistance - a.percentAssistance))
-  const upcomingEvents = getEventsRevenueAssistancePercent(data.events.filter(event => data.currentDate < event.date))
-  const pastEvents = getEventsRevenueAssistancePercent(data.events.filter(event => data.currentDate > event.date))
-  renderTableUpcomingPast(upcomingEvents, upcomingTable)
-  renderTableUpcomingPast(pastEvents, pastTable)
-  firstTable.innerHTML = template(highestAndLowestEvents[0].name, highestAndLowestEvents[1].name, eventWithHighestCapacity.name)
+  const eventsModule = new DataModule(data.events, data.currentDate)
+  const highestPercentEvent = eventsModule.getHighestPercentEvent()
+  const lowestPercentEvent = eventsModule.getLowestPercentEvent()
+  const largestCapacity = eventsModule.getEventsWithCapacity()[0].name
+  const upcomingStatics = eventsModule.getUpcomingEventsStatics()
+  const pastStatics = eventsModule.getPastEventsStatics()
+  firstTable.innerHTML = template(highestPercentEvent, lowestPercentEvent, largestCapacity)
+  renderTableUpcomingPast(upcomingStatics, upcomingTable)
+  renderTableUpcomingPast(pastStatics, pastTable)
 })
-const getPercent = (total, assistance) => ((assistance * 100)/total).toFixed(2)
-const getEventsWithPercent = (events) => events.filter((event) => event.assistance).map(event => ({name: event.name,  percentAssistance: getPercent(event.capacity, event.assistance)}))
-const getEventsWithCapacity = (events) => events.map((event) => ({name: event.name, capacity: event.capacity}))
-const getHighestAndLowestEvents = (events) => events.slice(0,1).concat(events.slice(-1))
-const eventRevenue = (assistance, price) => (price * assistance)
-const getEventsRevenueAssistancePercent = (events) => events.map(event => ({name: event.name, revenue: eventRevenue(event.assistance ? event.assistance : event.estimate, event.price), percentAssistance: getPercent(event.capacity, event.assistance ? event.assistance : event.estimate)}))
-const template = (firstElement, secondElement, thirdElement) => {
-  let html = `
-    <tr class="border border-1 text-center">
-      <td class="border border-1">${firstElement}</td>
-      <td class="border border-1">${secondElement}</td>
-      <td class="border border-1">${thirdElement}</td>
-    </tr>
-  `
-  return html
-}
-const renderTableUpcomingPast = (array, container) => {
-  let html = ''
-  array.forEach(element => {
-    html += template(element.name, `$${element.revenue}`, `${element.percentAssistance}%`)
-  });
-  container.innerHTML = html
-}
