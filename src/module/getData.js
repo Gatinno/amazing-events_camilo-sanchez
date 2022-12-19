280ec1ba8e2c887132c840298c8bf9803d539605
laptinho <camsanbar@gmail.com>
2022-12-19 11:25:21 -0300

    change error message from getData

--- src/module/getData.js
+++ src/module/getData.js
@@ -1,7 +1,7 @@
 const getData = async (url) => {
   const response = await fetch(url)
   if (!response.ok) {
-    throw new Error('ERROR:', response.status)
+    throw new Error(response.status)
   }
   const data = await response.json()
   return data
