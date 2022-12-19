444b67b55650f7d8bda628ea93eb11b87684090d
laptinho <camsanbar@gmail.com>
2022-12-19 14:58:36 -0300

    change past controler

--- src/controlers/past.js
+++ src/controlers/past.js
@@ -1,35 +1,35 @@
 import getData from "../module/getData.js";
 import renderCard from "../module/renderCard.js";
-import filterCategorySearch from "../module/filterCategorySearch.js";
 import renderCategoriesCheckbox from "../module/renderCategoriesCheckbox.js";
+import filterCategorySearch from "../module/filterCategorySearch.js";
+import DataModule from "../module/dataModule.js";
 document.addEventListener("DOMContentLoaded", async () => {
 	const cardContainer = document.getElementsByClassName("event-list")[0];
 	const checkboxContainer = document.getElementsByClassName("checkbox-filters")[0];
 	const searchInput = document.getElementsByClassName("search-input")[0];
-	const data = await getData("https://amazing-events.onrender.com/api/events");
+	const data = await getData("https://amazing-events.onrender.com/api/events")
+	const eventsModule = new DataModule(data.events, data.currentDate)
+	const eventsList = eventsModule.getPast()
 	const categories = [...new Set(data.events.map((event) => event.category))];
-	let activeCategories = []
+	let activeCategories = [];
 	renderCategoriesCheckbox(categories, checkboxContainer);
 	checkboxContainer.addEventListener('change', (e) => {
-			activeCategories = [...document.querySelectorAll('input[type="checkbox"]:checked')].map(elemento => elemento.value)
-			let filteredEvents = filterCategorySearch(data.events, activeCategories, searchInput.value)
-			renderCard(
-				{ currentDate: data.currentDate, events: filteredEvents },
-				cardContainer,
-				"past"
-			);
-		})
+		activeCategories = [...document.querySelectorAll('input[type="checkbox"]:checked')].map(elemento => elemento.value)
+		let filteredEvents  = filterCategorySearch(eventsList, activeCategories, searchInput.value)
+		renderCard(
+			{ events: filteredEvents },
+			cardContainer
+		);
+	})
 	renderCard(
-		{ currentDate: data.currentDate, events: data.events },
-		cardContainer,
-		"past"
+		{ events: eventsList },
+		cardContainer
 	);
 	searchInput.addEventListener("input", (e) =>{
-		let filteredEvents = filterCategorySearch(data.events, activeCategories, searchInput.value)
-			renderCard(
-				{ currentDate: data.currentDate, events: filteredEvents },
-				cardContainer,
-				"past"
-			);
+		let filteredEvents = filterCategorySearch(eventsList, activeCategories, searchInput.value)
+		renderCard(
+			{ events: filteredEvents },
+			cardContainer
+		);
 	});
 });
